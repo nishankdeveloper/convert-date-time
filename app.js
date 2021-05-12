c0fa9108d749328d6b20b3dbe168d9e69e8a6358
Nishank Gupta <72602029+nishankdeveloper@users.noreply.github.com>
2021-05-12 22:33:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ var app = express();
 app.get('/', function (req, res) {  
   res.send('Test Hello World');  
 });  
-var server = app.listen(8000, function () {  
-  var host = server.address().address;  
-  var port = server.address().port;  
-  console.log('Example app listening at http://%s:%s', host, port);  
-});
+
+const PORT = process.env.PORT || 5000
+
+var server = app.listen(PORT, function () {  
+  
+  var host = server.address().address  
+  var port = server.address().port  
+  console.log("Example app listening at http://%s:%s", host, port)  
+  
+})  
