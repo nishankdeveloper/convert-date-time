62cea4da3214cd51e731ef1f51f2bb6626032aa2
Nishank Gupta <72602029+nishankdeveloper@users.noreply.github.com>
2021-05-12 22:37:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ app.get('/', function (req, res) {
 
 const PORT = process.env.PORT || 5000
 
-var server = app.listen(PORT, function () {  
-  
-  var host = server.address().address  
-  var port = server.address().port  
-  console.log("Example app listening at http://%s:%s", host, port)  
-  
-})  
+var server = app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
