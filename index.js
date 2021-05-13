75ce97c12cb8c251363e87e952d3021f9e2f2104
Nishank Gupta <72602029+nishankdeveloper@users.noreply.github.com>
2021-05-13 13:20:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,21 @@ app.get('/', function(request, response) {
 
 app.get('/getCurrentDate', function (req, res) {
   var data = new Date();
+  var tzString = 'Asia/Kolkata';
+  
+  var date = (typeof date === "string" ? new Date(date) : date).toLocaleString("en-US", {timeZone: tzString})
+  
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+          console.log( 'strTime' );
+
+        console.log( strTime );
+
   
       console.log( data );
       res.send( data );
