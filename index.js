8a685ed6e48953eaa924903ae301624ab6b0f704
Nishank Gupta <72602029+nishankdeveloper@users.noreply.github.com>
2021-05-13 18:31:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get(endpoointVariable, function(request, response) {
-  
-  
-  console.log(endpoointVariable)
+app.get('/*', function(request, response) {
+
   console.log(request)
   console.log('request')
   
