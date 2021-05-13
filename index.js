21051769857d38a9c0feaa4939a81ca6bb601284
Nishank Gupta <72602029+nishankdeveloper@users.noreply.github.com>
2021-05-13 22:28:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/getCurrentDateTime', function (req, res) {
   
   try {
     
-        Date.setClientTimezoneOffset(300)
+     
 
     
     var clientTimezoneOffset = new Date().getTimezoneOffset()/60;//offset in hours
