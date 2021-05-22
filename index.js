31d40aaa4e8fddee62e86b93360d5191975108d7
Nishank Gupta <72602029+nishankdeveloper@users.noreply.github.com>
2021-05-22 22:11:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/dateTimeConversion', function (req, res) {
     var dateTime = req.query.dateTime;
     var convertTo = req.query.convertTo;
     var convertFrom = req != null && req != 'undefined' && req.query != null && req.query != 'undefined' && req.query.convertFrom != null && req.query.convertFrom != null ? req.query.convertFrom : null;
-    var utcOffset = "+00:00";   // by default
+    var utcOffset = convertTo != null && convertTo == 'undefined' && convertTo.toLocaleLowerCase() == 'utc' ? "+00:00" : null ;   // by default
 
     if(dateTime != null && dateTime != 'undefined' && convertTo != null && convertTo != 'undefined' && convertTo.toLocaleLowerCase() != 'utc'){
       
@@ -33,34 +33,38 @@ app.get('/dateTimeConversion', function (req, res) {
          }
       });
 
-      //Split the offset fetched into hours and minutes.
-      var a = utcOffset.split(':');
-      var hour = +a[0];
-      var minute = utcOffset[0] == '-' ? -1 * +a[1] : +a[1] ;
+      if(utcOffset == null){
+        body["success"] = false;
+        body["message"] = "Bad value for convertTo param. Use only GMT or UTC.";
+      }else{
+        //Split the offset fetched into hours and minutes.
+        var a = utcOffset.split(':');
+        var hour = +a[0];
+        var minute = utcOffset[0] == '-' ? -1 * +a[1] : +a[1] ;
 
-      //Get the value in minutes for the offset.
-      var tzDifference = hour * 60 + minute;
+        //Get the value in minutes for the offset.
+        var tzDifference = hour * 60 + minute;
 
-      var targetTime = new Date(dateTime);
-     
-      //get the date in convertTo timezone.
-      targetTime = new Date(targetTime.getTime() + ( tzDifference * 60 * 1000));
+        var targetTime = new Date(dateTime);
 
-      //get the am/pm format time from the targetTime.
-      var hours = targetTime.getHours();
-      var minutes = targetTime.getMinutes();
-      var ampm = hours >= 12 ? 'pm' : 'am';
-      hours = hours % 12;
-      hours = hours ? hours : 12; // the hour '0' should be '12'
-      minutes = minutes < 10 ? '0'+minutes : minutes;
-      var strTime = hours + ':' + minutes + ' ' + ampm;
+        //get the date in convertTo timezone.
+        targetTime = new Date(targetTime.getTime() + ( tzDifference * 60 * 1000));
 
-      body["success"] = true;
-      body["date"] = targetTime.getFullYear() + "-" + ( (targetTime.getMonth()+1) < 10 ? '0'+(targetTime.getMonth()+1) : (targetTime.getMonth()+1))
-                     + "-" + ( targetTime.getDate() < 10 ? '0'+targetTime.getDate() : targetTime.getDate()) ;     //// date in YYYY-MM-DD format
-      body["time"] = strTime;
-      body["dateTime"] = targetTime.toISOString();
+        //get the am/pm format time from the targetTime.
+        var hours = targetTime.getHours();
+        var minutes = targetTime.getMinutes();
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        minutes = minutes < 10 ? '0'+minutes : minutes;
+        var strTime = hours + ':' + minutes + ' ' + ampm;
 
+        body["success"] = true;
+        body["date"] = targetTime.getFullYear() + "-" + ( (targetTime.getMonth()+1) < 10 ? '0'+(targetTime.getMonth()+1) : (targetTime.getMonth()+1))
+                       + "-" + ( targetTime.getDate() < 10 ? '0'+targetTime.getDate() : targetTime.getDate()) ;     //// date in YYYY-MM-DD format
+        body["time"] = strTime;
+        body["dateTime"] = targetTime.toISOString();
+      }
 
     }else if(dateTime != null && dateTime != 'undefined' && convertTo != null && convertTo != 'undefined' && convertTo.toLocaleLowerCase() == 'utc' && 
             convertFrom != null && convertFrom != 'undefined'){
